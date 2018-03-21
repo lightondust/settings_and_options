9d4cb5fecfb3c1cbfc4897c07ad3c5294473b923
zs mcp <choumori@choumorinoMacBook-puro.local>
2018-03-21 14:08:15 +0900

    neural.sh

---
+++ neural.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#git clone https://github.com/lightondust/settings_and_options.git
+#c.NotebookApp.ip = '0.0.0.0'をファイルに追加
+echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
+
+
